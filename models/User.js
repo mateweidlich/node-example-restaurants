bf03264ade6763aea0d00554b8cd8e7892bb183e
Mate Weidlich <weidlichmate@gmail.com>
2018-07-28 22:46:49 +0200

    app

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid email address'],
+    required: 'Please supply an email address'
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please supply a name'
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  hearts: [{ type: mongoose.Schema.ObjectId, ref: 'Store' }]
+});
+
+userSchema.virtual('gravatar').get(function() {
+  const hash = md5(this.email);
+  return `https://gravatar.com/avatar/${hash}?s=200`;
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
